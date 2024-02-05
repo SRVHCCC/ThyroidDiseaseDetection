87cafbb8a979e716e4efc868d7763361ddb14551
himanshu <you@example.com>
2024-02-05 16:46:44 +0530

    Your commit message here

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+from typing import List
+
+setup(
+    name='ThyroidDiseaseDetection',
+    version='0.0.1',
+    author="Himanshu",
+    author_email='chaurasiyahimanshu2002@gmail.com',
+    packages=find_packages()
+)
+
